422d73c847196f72286209acd5d3666e762555aa
Matteo De Donno <matteodedonno02@gmail.com>
2021-07-28 15:15:25 +0200

    Created description list that show fetched data from api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react"
 import CustomNavbar from "./components/CustomNavbar"
+import DescriptionList from "./components/DescriptionList"
 
 export default function App() {
     return <>
         <CustomNavbar/>
+        <DescriptionList/>
     </>
 }
---
+++ src/components/DescriptionList.js
@@ -0,0 +1,44 @@
+/* This example requires Tailwind CSS v2.0+ */
+
+import { useEffect, useState } from "react"
+
+export default function Example() {
+    const [data, setData] = useState({})
+
+    useEffect(() => {
+        fetch("https://api.covid19api.com/country/italy")
+            .then(response => response.json())
+            .then(data => setData(data[data.length - 1]))
+    }, [])
+
+    return (
+        <div className="container px-20">
+            <div className="bg-white shadow overflow-hidden sm:rounded-lg">
+                <div className="px-4 py-5 sm:px-6">
+                    <h3 className="text-lg leading-6 font-medium text-gray-900">Dati Covid</h3>
+                    <p className="mt-1 max-w-2xl text-sm text-gray-500">Dati relativi al giorno</p>
+                </div>
+                <div className="border-t border-gray-200">
+                    <dl>
+                        <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium text-gray-500">Casi confermati</dt>
+                            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{data.Confirmed}</dd>
+                        </div>
+                        <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium text-gray-500">Morti</dt>
+                            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{data.Deaths}</dd>
+                        </div>
+                        <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium text-gray-500">Ricoveri</dt>
+                            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{data.Recovered}</dd>
+                        </div>
+                        <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt className="text-sm font-medium text-gray-500">Casi attivi</dt>
+                            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{data.Active}</dd>
+                        </div>
+                    </dl>
+                </div>
+            </div>
+        </div>
+    )
+}
